bb4df8ca4b89c6cd61a320110083be93867be3a7
mewing <anamewing@gmail.com>
2012-09-03 04:45:11 +0800

    完成输入输出文件记录
    Signed-off-by: mewing <anamewing@gmail.com>

--- Form1.cs
+++ Form1.cs
@@ -11,20 +11,45 @@ namespace SortDesktopIcon
 {
     public partial class Form1 : Form
     {
+        IList<IcoObj> icoObj;
+        IDictionary<string, IcoObj> icoDics;
+        DesktopIcon myDesktopIcon;
         public Form1()
         {
             InitializeComponent();
+            myDesktopIcon = new DesktopIcon();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            IList<IcoObj> icoObj;
-            IList<Point> pointArr=new List<Point>();
-            DesktopIcon myDesktopIcon=new DesktopIcon();
+            
+            //IList<Point> pointArr=new List<Point>();
             icoObj = myDesktopIcon.getICO();
-            int i= 0;
-            pointArr.Add(new Point(200, 200));
-            WinAPI.SendMessage(icoObj[i].itemIntPtr, WinAPI.LVM_SETITEMPOSITION, i, WinAPI.MAKELPARAM(pointArr[i].X, pointArr[i].Y));
+            
+            configfile myconfig = new configfile(icoObj);
+            myconfig.writefile();
+            //int i= 0;
+            //pointArr.Add(new Point(200, 200));
+            //WinAPI.SendMessage(icoObj[i].itemIntPtr, WinAPI.LVM_SETITEMPOSITION, i, WinAPI.MAKELPARAM(pointArr[i].X, pointArr[i].Y));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            icoObj = myDesktopIcon.getICO();
+            configfile myconfig = new configfile(icoObj);
+            icoDics = myconfig.readfile();
+            for (int i=0;i<icoObj.Count;i++)
+            {
+                IcoObj ico=icoObj[i];
+                if (icoDics.ContainsKey(ico.name))
+                {
+                    Point newPoint = icoDics[ico.name].itemPoint;
+                    WinAPI.SendMessage(ico.itemIntPtr, WinAPI.LVM_SETITEMPOSITION, i, WinAPI.MAKELPARAM(newPoint.X, newPoint.Y));
+                    icoDics.Remove(ico.name);
+
+                }
+            }
         }
 
         
---
+++ configfile.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace SortDesktopIcon
+{
+    class configfile
+    {
+        string filename = "config.txt";
+        IList<IcoObj> icoObjs;
+        
+
+        public configfile(IList<IcoObj> vicoObj)
+        {
+            icoObjs = vicoObj;
+
+        }
+
+        public bool writefile()
+        {
+            bool succeed = true;
+            try
+            {
+                StreamWriter sw1 = new StreamWriter(filename);
+                foreach (IcoObj ico in icoObjs)
+                {
+                    string line = ico.name + ";"  + ico.itemPoint.X.ToString() + ";" + ico.itemPoint.Y.ToString();
+                    sw1.WriteLine(line);
+                }
+                sw1.Close();
+            }
+            catch
+            {
+                succeed = false;
+            }
+            return succeed;
+        }
+
+        public IDictionary<string, IcoObj> readfile()
+        {
+            IDictionary<string, IcoObj> icoDics=new Dictionary<string ,IcoObj>();
+            bool succeed = true;
+            try
+            {
+                StreamReader sr1 = new StreamReader(filename);
+                while (sr1.Peek() >= 0)
+                {
+                    string line = sr1.ReadLine();
+                    string[] paras = line.Split(';');
+                    if (paras.Length != 3)
+                    {
+                        Trace.WriteLine(line);
+                        continue;
+                    }
+                    IcoObj ico = new IcoObj(paras[0], new IntPtr(), new Point(Convert.ToInt32(paras[1]), Convert.ToInt32(paras[2])));
+                    icoDics.Add(paras[0], ico);
+
+                }
+                sr1.Close();
+            }
+            catch
+            {
+                succeed = false;
+            }
+            return icoDics;
+        }
+        
+        
+
+    }
+}
