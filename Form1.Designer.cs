123b3ac80c43bf622c024686f838555cc36d625d
mewing <anamewing@gmail.com>
2012-09-04 00:52:00 +0800

    美化外观
    Signed-off-by: mewing <anamewing@gmail.com>

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 42);
+            this.button1.Location = new System.Drawing.Point(12, 119);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(85, 23);
             this.button1.TabIndex = 0;
@@ -48,7 +48,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(143, 42);
+            this.button2.Location = new System.Drawing.Point(133, 130);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(85, 23);
             this.button2.TabIndex = 2;
@@ -58,7 +58,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(48, 11);
+            this.button3.Location = new System.Drawing.Point(22, 42);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
@@ -73,7 +73,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(143, 11);
+            this.button4.Location = new System.Drawing.Point(144, 42);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 4;
